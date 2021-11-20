03550439bdb5596efc64bf297b10d2a0c7f793b0
Xiangfeng Zhu <xzhu0027@gmail.com>
2021-11-19 23:17:58 -0800

    tweaks

---
+++ hotelReservation/parse_log.py
@@ -0,0 +1,16 @@
+import re
+import numpy as np
+
+file_name = 'logs.txt'
+target = 'userClient.CheckUser'
+
+result = []
+with open(file_name, 'r') as fin:
+    lines = fin.readlines()
+    for l in lines:
+        if target in l:
+            result.append(float(re.findall(r"\d+\.\d+", l)[0]))
+
+latency = np.array(result)
+print(np.percentile(latency, 50))
+
