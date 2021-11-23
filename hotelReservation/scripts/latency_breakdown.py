e63d328c177ffd071a8f3630d0138165010f34c3
Xiangfeng Zhu <xzhu0027@gmail.com>
2021-11-22 22:48:38 -0800

    tweaks

--- hotelReservation/scripts/latency_breakdown.py
+++ hotelReservation/scripts/latency_breakdown.py
@@ -63,6 +63,20 @@ def run_tcp_proxy_latency_breakdown(app, envoy_process, duration):
 
     return breakdown
 
+def run_http_proxy_latency_breakdown(app, envoy_process, duration):
+    print("Running " + str(app) + " latency breakdown...")
+    breakdown = {}
+    breakdown['loopback_latency'] = run_funclatency('process_backlog', duration, envoy_process['envoy_pid'])
+    breakdown['read_latency'] = run_funclatency('do_readv', duration, envoy_process['envoy_pid'])
+    breakdown['write_latency'] = run_funclatency('do_writev', duration, envoy_process['envoy_pid']) - breakdown['loopback_latency']
+    breakdown['epoll_latency'] = run_funclatency('ep_send_events_proc', duration, envoy_process['envoy_pid'])
+    http2_latency = breakdown['envoy_latency'] = run_funclatency(envoy_process['envoy_binary_path']+':*nghttp2_session_mem_recv*', duration, envoy_process['envoy_pid'])
+    http_latency = breakdown['envoy_latency'] = run_funclatency(envoy_process['envoy_binary_path']+':*http_parser_execute*', duration, envoy_process['envoy_pid'])
+    breakdown['envoy_latency'] = run_funclatency(envoy_process['envoy_binary_path']+':*onReadReady*', 
+                        duration, envoy_process['envoy_pid'], delta_min=max(http_latency, http2_latency)) - breakdown['read_latency']
+    breakdown['envoy_latency'] += run_funclatency(envoy_process['envoy_binary_path']+':*onWriteReady*', 
+                        duration, envoy_process['envoy_pid'], delta_min=breakdown['write_latency']) - breakdown['write_latency']
+
 if __name__ == '__main__':
     args = parse_args()
 
@@ -72,6 +86,10 @@ if __name__ == '__main__':
     result = {}
     for app, envoy_process in envoy_info.items():
         if args.proxy == "tcp":
+            print("Running breakdown experiment for TCP proxy...")
             result[app] = run_tcp_proxy_latency_breakdown(app, envoy_process, 30)
+        elif args.proxy == "http":
+            print("Running breakdown experiment for HTTP proxy...")
+            result[app] = run_http_proxy_latency_breakdown(app, envoy_process, 30)
 
     print(result)
