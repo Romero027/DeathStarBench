fa1046b9ac4403e65d6875175bb8a579d32d5c00
Xiangfeng(Allen) Zhu <xzhu0027@gmail.com>
2021-12-09 11:59:51 -0800

    tweaks

---
+++ setup_scripts/misc.sh
@@ -0,0 +1,3 @@
+# client-side throttling, not priority and fairness problem
+# Source: https://stackoverflow.com/questions/66339069/kubectl-get-all-command-return-throttling-request
+sudo chmod 777 -R ~/.kube/cache
--- setup_scripts/update_kernel.sh
+++ setup_scripts/update_kernel.sh
@@ -12,35 +12,46 @@
 # CONFIG_CGROUP_BPF=y
 # sed -i 's/CONFIG_BPF=y/new-text/g' .config
 
-function update_kernel() {
-    KERNEL_VERSION=$1
-    IP=$2
-    NUMBER_OF_CORES=$3
-ssh $2 > /dev/null 2>&1 << eeooff
-    wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$KERNEL_VERSION.tar.xz
-    unxz -v linux-$KERNEL_VERSION.tar.xz
-    tar xvf linux-$KERNEL_VERSION.tar
-eeooff
 
-scp .config $2:~/linux-$KERNEL_VERSION
 
-ssh $2 > /dev/null 2>&1 << eeooff
-    cd linux-$KERNEL_VERSION
-    sudo apt-get update
-    sudo apt-get install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev
-    sudo make menuconfig
-    # add CONFIG_SYSTEM_TRUSTED_KEYS="" to .config
-    sudo make -j $(nproc)
-    sudo make modules_install
-    sudo make install
-    sudo update-initramfs -c -k $KERNEL_VERSION
-    
-    sudo update-grub
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.10.76.tar.xz
+unxz -v linux-5.10.76.tar.xz
+tar xvf linux-5.10.76.tar
 
-    cd tools/perf
-    sudo make -j $(nproc)
-    sudo cp perf /usr/bin/
-    sudo reboot
-eeooff
-}
+cd linux-5.10.76
+sudo apt-get update
+sudo apt-get install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev
+sudo make menuconfig
+# add CONFIG_SYSTEM_TRUSTED_KEYS="" to .config
+#echo CONFIG_SYSTEM_TRUSTED_KEYS="" >> .config
+sudo make -j $(nproc)
+sudo make modules_install
+sudo make install
+sudo update-initramfs -c -k 5.10.76
 
+sudo update-grub
+
+cd tools/perf
+sudo make -j $(nproc)
+sudo cp perf /usr/bin/
+sudo reboot
+
+# install BCC
+# For 18.04
+sudo apt-get -y install bison build-essential cmake flex git libedit-dev \
+  libllvm6.0 llvm-6.0-dev libclang-6.0-dev python zlib1g-dev libelf-dev libfl-dev python3-distutils
+
+# For 20.04.1
+sudo apt install -y bison build-essential cmake flex git libedit-dev \
+  libllvm7 llvm-7-dev libclang-7-dev python zlib1g-dev libelf-dev libfl-dev python3-distutils
+
+git clone https://github.com/iovisor/bcc.git
+mkdir bcc/build; cd bcc/build
+cmake ..
+make
+sudo make install
+cmake -DPYTHON_CMD=python3 .. # build python3 binding
+pushd src/python/
+make
+sudo make install
+popd
