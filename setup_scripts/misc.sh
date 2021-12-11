be89c08aee9690ec18920c34631f647cd0c581c8
Xiangfeng Zhu <xzhu0027@gmail.com>
2021-12-10 17:02:04 -0800

    tweaks

--- setup_scripts/misc.sh
+++ setup_scripts/misc.sh
@@ -1,3 +1,7 @@
 # client-side throttling, not priority and fairness problem
 # Source: https://stackoverflow.com/questions/66339069/kubectl-get-all-command-return-throttling-request
 sudo chmod 777 -R ~/.kube/cache
+
+# Build and push docker image
+docker build -t xzhu0027/online_boutique_frontend:latest -f Dockerfile .
+docker push xzhu0027/online_boutique_frontend:latest 
