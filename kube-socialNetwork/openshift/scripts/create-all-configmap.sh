cec78f6add74f5e23268afb0e01c1415cd9e6868
Xiangfeng Zhu <xzhu0027@gmail.com>
2021-12-11 00:04:35 -0800

    tweaks

--- kube-socialNetwork/openshift/scripts/create-all-configmap.sh
+++ kube-socialNetwork/openshift/scripts/create-all-configmap.sh
@@ -5,5 +5,6 @@ cd $(dirname $0)/..
 ./scripts/configmaps/create-jaeger-configmap.sh
 ./scripts/configmaps/create-media-frontend-configmap.sh
 ./scripts/configmaps/create-nginx-thrift-configmap.sh
+kubectl create cm service-config-json --from-file=config/service-config.json  -n social-network
 
 cd - >/dev/null
