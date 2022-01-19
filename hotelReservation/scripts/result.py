6580ba85d317c3d00bf394b42a0fe8755c3a18ad
Xiangfeng Zhu <xzhu0027@gmail.com>
2022-01-18 23:01:46 -0800

    s

---
+++ hotelReservation/scripts/result.py
@@ -0,0 +1,90 @@
+s1=[
+{'user': {'loopback_latency': 10712, 'read_latency': 2403, 'write_latency': 7813, 'epoll_latency': 1471, 'http2_latency': 67789, 'envoy_latency': 137424, 'http_latency': 78812}},
+{'user': {'loopback_latency': 10575, 'read_latency': 2535, 'write_latency': 7020, 'epoll_latency': 1428, 'http2_latency': 65759, 'envoy_latency': 178029, 'http_latency': 119008}},
+{'user': {'loopback_latency': 10640, 'read_latency': 2540, 'write_latency': 7510, 'epoll_latency': 1428, 'http2_latency': 65151, 'envoy_latency': 178673, 'http_latency': 122864}},
+] # 0.88
+
+s100=[
+{'user': {'loopback_latency': 10657, 'read_latency': 2611, 'write_latency': 7537, 'epoll_latency': 1471, 'http2_latency': 70466, 'envoy_latency': 142306, 'http_latency': 76372}},
+{'user': {'loopback_latency': 10562, 'read_latency': 2524, 'write_latency': 8343, 'epoll_latency': 1477, 'http2_latency': 66802, 'envoy_latency': 141216, 'http_latency': 88718}},
+{'user': {'loopback_latency': 10432, 'read_latency': 2576, 'write_latency': 7537, 'epoll_latency': 1461, 'http2_latency': 69701, 'envoy_latency': 204685, 'http_latency': 140491}}
+] #0.91
+
+s200=[
+{'user': {'loopback_latency': 10675, 'read_latency': 2608, 'write_latency': 7892, 'epoll_latency': 1461, 'http2_latency': 68111, 'envoy_latency': 216432, 'http_latency': 179517}},
+{'user': {'loopback_latency': 10455, 'read_latency': 2509, 'write_latency': 7339, 'epoll_latency': 1413, 'http2_latency': 66478, 'envoy_latency': 141354, 'http_latency': 82370}},
+{'user': {'loopback_latency': 10690, 'read_latency': 2593, 'write_latency': 7847, 'epoll_latency': 1484, 'http2_latency': 69712, 'envoy_latency': 145046, 'http_latency': 73379}}
+] 
+
+s300=[
+{'user': {'loopback_latency': 10715, 'read_latency': 2580, 'write_latency': 7424, 'epoll_latency': 1469, 'http2_latency': 68967, 'envoy_latency': 147997, 'http_latency': 104609}},
+{'user': {'loopback_latency': 10963, 'read_latency': 2558, 'write_latency': 7508, 'epoll_latency': 1453, 'http2_latency': 68478, 'envoy_latency': 171466, 'http_latency': 114353}},
+{'user': {'loopback_latency': 10723, 'read_latency': 2581, 'write_latency': 7955, 'epoll_latency': 1475, 'http2_latency': 70337, 'envoy_latency': 156684, 'http_latency': 71459}}    
+]
+
+s400=[
+{'user': {'loopback_latency': 10682, 'read_latency': 2551, 'write_latency': 7794, 'epoll_latency': 1465, 'http2_latency': 71387, 'envoy_latency': 149454, 'http_latency': 71155}},
+{'user': {'loopback_latency': 10897, 'read_latency': 2538, 'write_latency': 7352, 'epoll_latency': 1401, 'http2_latency': 70854, 'envoy_latency': 146440, 'http_latency': 81189}},
+{'user': {'loopback_latency': 10914, 'read_latency': 2563, 'write_latency': 7394, 'epoll_latency': 1483, 'http2_latency': 70389, 'envoy_latency': 197150, 'http_latency': 133698}}
+]
+
+s500=[
+{'user': {'loopback_latency': 10851, 'read_latency': 2647, 'write_latency': 7886, 'epoll_latency': 1445, 'http2_latency': 68228, 'envoy_latency': 215272, 'http_latency': 156848}},
+{'user': {'loopback_latency': 10694, 'read_latency': 2419, 'write_latency': 7750, 'epoll_latency': 1496, 'http2_latency': 69353, 'envoy_latency': 182671, 'http_latency': 124065}},
+{'user': {'loopback_latency': 10918, 'read_latency': 2622, 'write_latency': 7978, 'epoll_latency': 1466, 'http2_latency': 69888, 'envoy_latency': 164564, 'http_latency': 111147}},
+]
+
+s600=[
+{'user': {'loopback_latency': 10982, 'read_latency': 2642, 'write_latency': 7921, 'epoll_latency': 1491, 'http2_latency': 69135, 'envoy_latency': 182312, 'http_latency': 121520}},
+{'user': {'loopback_latency': 10920, 'read_latency': 2683, 'write_latency': 7093, 'epoll_latency': 1460, 'http2_latency': 68471, 'envoy_latency': 143975, 'http_latency': 84568}},
+{'user': {'loopback_latency': 10835, 'read_latency': 2382, 'write_latency': 6217, 'epoll_latency': 1488, 'http2_latency': 70852, 'envoy_latency': 160419, 'http_latency': 107912}}
+]
+
+s700=[
+{'user': {'loopback_latency': 10488, 'read_latency': 2570, 'write_latency': 7840, 'epoll_latency': 1465, 'http2_latency': 68831, 'envoy_latency': 149841, 'http_latency': 93742}},
+{'user': {'loopback_latency': 10779, 'read_latency': 2561, 'write_latency': 7791, 'epoll_latency': 1462, 'http2_latency': 70169, 'envoy_latency': 211438, 'http_latency': 144842}},
+{'user': {'loopback_latency': 10813, 'read_latency': 2484, 'write_latency': 7456, 'epoll_latency': 1466, 'http2_latency': 69954, 'envoy_latency': 183056, 'http_latency': 123692}}
+] #0.94
+
+s800=[
+{'user': {'loopback_latency': 11098, 'read_latency': 2709, 'write_latency': 7598, 'epoll_latency': 1486, 'http2_latency': 72890, 'envoy_latency': 147753, 'http_latency': 86765}},
+{'user': {'loopback_latency': 10957, 'read_latency': 2674, 'write_latency': 7505, 'epoll_latency': 1490, 'http2_latency': 68769, 'envoy_latency': 149685, 'http_latency': 92089}},
+{'user': {'loopback_latency': 10992, 'read_latency': 2585, 'write_latency': 7597, 'epoll_latency': 1465, 'http2_latency': 68184, 'envoy_latency': 173087, 'http_latency': 117611}}
+] #0.95
+
+s900=[
+{'user': {'loopback_latency': 10949, 'read_latency': 2672, 'write_latency': 7689, 'epoll_latency': 1531, 'http2_latency': 69806, 'envoy_latency': 147694, 'http_latency': 99384}},
+{'user': {'loopback_latency': 10816, 'read_latency': 2655, 'write_latency': 7875, 'epoll_latency': 1478, 'http2_latency': 68794, 'envoy_latency': 208800, 'http_latency': 148454}},
+{'user': {'loopback_latency': 10722, 'read_latency': 2514, 'write_latency': 7827, 'epoll_latency': 1460, 'http2_latency': 69364, 'envoy_latency': 151082, 'http_latency': 76898}}
+] #0.94
+
+s1000=[
+{'user': {'loopback_latency': 10995, 'read_latency': 2674, 'write_latency': 7494, 'epoll_latency': 1507, 'http2_latency': 69942, 'envoy_latency': 216381, 'http_latency': 172876}},
+{'user': {'loopback_latency': 11063, 'read_latency': 2705, 'write_latency': 7801, 'epoll_latency': 1491, 'http2_latency': 70681, 'envoy_latency': 148980, 'http_latency': 87793}},
+{'user': {'loopback_latency': 10954, 'read_latency': 2718, 'write_latency': 7934, 'epoll_latency': 1450, 'http2_latency': 68587, 'envoy_latency': 180552, 'http_latency': 120608}}
+] #0.97
+
+
+s1100=[
+{'user': {'loopback_latency': 10909, 'read_latency': 2682, 'write_latency': 7921, 'epoll_latency': 1483, 'http2_latency': 69984, 'envoy_latency': 202616, 'http_latency': 141266}},
+{'user': {'loopback_latency': 11011, 'read_latency': 2686, 'write_latency': 7354, 'epoll_latency': 1422, 'http2_latency': 67614, 'envoy_latency': 167969, 'http_latency': 116529}},
+{'user': {'loopback_latency': 10798, 'read_latency': 2684, 'write_latency': 7787, 'epoll_latency': 1427, 'http2_latency': 71167, 'envoy_latency': 200391, 'http_latency': 137405}}    
+]
+
+s1200=[
+{'user': {'loopback_latency': 11253, 'read_latency': 2711, 'write_latency': 7765, 'epoll_latency': 1463, 'http2_latency': 71107, 'envoy_latency': 149346, 'http_latency': 81239}},
+{'user': {'loopback_latency': 11046, 'read_latency': 2668, 'write_latency': 7775, 'epoll_latency': 1494, 'http2_latency': 72354, 'envoy_latency': 148526, 'http_latency': 103552}},
+{'user': {'loopback_latency': 11215, 'read_latency': 2641, 'write_latency': 7842, 'epoll_latency': 1491, 'http2_latency': 70994, 'envoy_latency': 169760, 'http_latency': 112559}}    
+]
+
+s1300=[
+{'user': {'loopback_latency': 10995, 'read_latency': 2671, 'write_latency': 8302, 'epoll_latency': 1434, 'http2_latency': 70211, 'envoy_latency': 168381, 'http_latency': 115357}},
+{'user': {'loopback_latency': 10864, 'read_latency': 2719, 'write_latency': 8502, 'epoll_latency': 1471, 'http2_latency': 69857, 'envoy_latency': 173605, 'http_latency': 118426}},
+{'user': {'loopback_latency': 11125, 'read_latency': 2746, 'write_latency': 7460, 'epoll_latency': 1548, 'http2_latency': 70243, 'envoy_latency': 204244, 'http_latency': 140073}}    
+]
+
+s1400=[
+{'user': {'loopback_latency': 10474, 'read_latency': 4598, 'write_latency': 7784, 'epoll_latency': 1645, 'http2_latency': 76630, 'envoy_latency': 210151, 'http_latency': 139846}},
+{'user': {'loopback_latency': 10282, 'read_latency': 4386, 'write_latency': 7638, 'epoll_latency': 1485, 'http2_latency': 70100, 'envoy_latency': 229551, 'http_latency': 160850}},
+{'user': {'loopback_latency': 10507, 'read_latency': 4600, 'write_latency': 7890, 'epoll_latency': 1502, 'http2_latency': 68584, 'envoy_latency': 196414, 'http_latency': 134567}}
+]
