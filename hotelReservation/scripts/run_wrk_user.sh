9d9a5f237ad7ea1adeff81ce0811e848ffcfcc8d
hazelnut <hazelnut@localhost>
2022-01-28 01:07:18 -0500

    ads

---
+++ hotelReservation/scripts/run_wrk_user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#scales=(100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500)
+scales=(100)
+for s in ${scales[@]}; do
+    echo "Running scale $s"
+    for i in {1..5}
+    do
+      ./wrk/wrk -t1 -c1 -d 300s -s ../wrk2/scripts/hotel-reservation/user_workload/user_workload_$s.lua http://10.96.7.56:5000 --latency > /dev/null 2>&1 &
+      wrk_pid=$!
+      echo $wrk_pid
+      ssh h2 "sudo python3 /users/hazelnut/DeathStarBench/hotelReservation/scripts/latency_breakdown.py --proxy http -n 9000 -d 10"
+      kill -9 $wrk_pid
+      sleep 5
+    done
+done
+
+echo "Running scale 0"
+
+
