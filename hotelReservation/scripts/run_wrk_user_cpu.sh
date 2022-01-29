f0088cb021a2db4cbb8b4b829f75477e28d34567
Xiangfeng Zhu <xzhu0027@gmail.com>
2022-01-29 00:08:32 -0800

    tweaks

---
+++ hotelReservation/scripts/run_wrk_user_cpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+scales=(100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500)
+
+for s in ${scales[@]}; do
+    echo "Running scale $s"
+    for i in {1..5}
+    do
+      ./wrk2/wrk -t2 -c100 -d300s -s echo-server/wrk-script/request_b/echo_workload_$.lua http://10.96.88.88:80 -R 60000 > /dev/null 2>&1 &
+      sleep 10
+      wrk_pid=$!
+      echo $wrk_pid
+      ssh h2 "sudo python3 /users/hazelnut/DeathStarBench/hotelReservation/scripts/cpu.py"
+      kill -9 $wrk_pid
+      sleep 5
+    done
+done
+
+echo "Running scale 0"
