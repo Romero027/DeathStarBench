cbe79f78e4390b12cf0d9e287f88c08beefe19e4
Xiangfeng Zhu <xzhu0027@gmail.com>
2022-01-30 15:47:40 -0800

    tweaks

--- hotelReservation/scripts/cpu_breakdown.py
+++ hotelReservation/scripts/cpu_breakdown.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--proxy', type=str, default='tcp', help='proxy type (tcp, http or grpc')
+    parser.add_argument('--proxy', type=str, default='tcp', help='proxy type (none, tcp, http or grpc)')
     parser.add_argument('--app', type=str, help='the name of the application', required=True)
     parser.add_argument("-v", "--verbose", action="store_true", help="print the command executed (for debugging purposes)")
     return parser.parse_args()
@@ -59,12 +59,13 @@ def generate_flamegraph():
 def get_cpu_breakdown(virtual_cores, proxy, app):
     print("Caculating CPU breakdown...")
     breakdown = {}
-    breakdown['read'] = virtual_cores*get_cpu_percentage(">readv (")*0.01
-    breakdown['loopback'] = virtual_cores*get_cpu_percentage(">process_backlog (")*0.01
-    breakdown['write'] = virtual_cores*get_cpu_percentage(">writev (")*0.01 - breakdown['loopback']
-    breakdown['epoll'] = virtual_cores*get_cpu_percentage(">epoll_wait (")*0.01
-    breakdown['envoy'] = virtual_cores*get_cpu_percentage(">wrk:worker_0 (")*0.01+virtual_cores*get_cpu_percentage(">wrk:worker_1 (")*0.01
-    breakdown['envoy'] = breakdown['envoy']-(breakdown['read']+breakdown['write']+breakdown['loopback']+breakdown['epoll'])
+    if proxy != "none":
+        breakdown['read'] = virtual_cores*get_cpu_percentage(">readv (")*0.01
+        breakdown['loopback'] = virtual_cores*get_cpu_percentage(">process_backlog (")*0.01
+        breakdown['write'] = virtual_cores*get_cpu_percentage(">writev (")*0.01 - breakdown['loopback']
+        breakdown['epoll'] = virtual_cores*get_cpu_percentage(">epoll_wait (")*0.01
+        breakdown['envoy'] = virtual_cores*get_cpu_percentage(">wrk:worker_0 (")*0.01+virtual_cores*get_cpu_percentage(">wrk:worker_1 (")*0.01
+        breakdown['envoy'] = breakdown['envoy']-(breakdown['read']+breakdown['write']+breakdown['loopback']+breakdown['epoll'])
     breakdown['app'] = virtual_cores*get_cpu_percentage(">"+app+" (")*0.01
     if proxy == 'http' or proxy =='grpc':
         breakdown['http'] = virtual_cores*get_cpu_percentage(">Envoy::Network::FilterManagerImpl::onContinueReading(")*0.01
